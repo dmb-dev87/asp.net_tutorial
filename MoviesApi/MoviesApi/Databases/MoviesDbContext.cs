cf2ea5d0209f3c8d293a04d3309b43ed2c595cda
Bojan <dmbdev800@gmail.com>
2021-07-29 02:42:45 -0400

    [dmb-dev87][2021-07-29][add EntityFramework API tutorial]

---
+++ MoviesApi/MoviesApi/Databases/MoviesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApi.Models;
+
+namespace MoviesApi.Databases
+{
+    public class MoviesDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public MoviesDbContext(DbContextOptions<MoviesDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movie");
+        }
+    }
+}
