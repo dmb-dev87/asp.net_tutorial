2be59456eb1e410e929ab9fe755b375650e6bd17
Bojan <dmbdev800@gmail.com>
2021-08-30 05:34:08 -0400

    [dmb-dev87][2021-08-30][add JWTAuth project]

---
+++ JWTAuth/JWTAuth/Authentication/AuthenticationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace JWTAuth.Authentication
+{
+    public class AuthenticationUser : IdentityUser
+    {
+    }
+}
