3168c9a7673bb809f02c62ba72b08f30e5a64970
dmb-dev87 <dmbdev800@gmail.com>
2021-11-23 01:04:15 -0500

    [dmb-dev87][2021-11-23][add MVVMDemo]

---
+++ MVVMDemo/MVVMDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMDemo.Model;
+
+namespace MVVMDemo.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+            Students = students;
+        }
+    }
+}
